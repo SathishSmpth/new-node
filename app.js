f7ab5f7781f8ee0d1b59e90dad823654f7a35789
Sathish Sampath <sathishsmpth@outlook.com>
2023-10-17 12:00:20 +0530

    final commit

--- app.js
+++ app.js
@@ -6,22 +6,20 @@ const globalError = require("./controller/errorController");
 
 const productsRoutes = require("./routes/productRoutes");
 const authRoutes = require("./routes/authRoutes");
-const { swaggerUi, swaggerSpec } = require("./config/swagger.config.js");
 
 const app = express();
 
-app.use(logger("dev"));  
-app.use(express.json());    
-app.use(cors());    
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/products", productsRoutes);
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Unhandled routes
 app.all("*", (req, res, next) => {
   const error = new Error(`Can't find ${req.originalUrl} on this server!`);
-  error.status = "fail";  
+  error.status = "fail";
   error.statusCode = 404;
   next(error);
 });
